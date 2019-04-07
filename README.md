5a47a7f4dc37357586073665019008eead78463c
Dsouza <347394@cognizant.com>
2019-04-07 13:45:03 +0100

    basic app

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# react-components
+# react-seed-webpack
+Prerequisit
+1. Node 8.10.0 or later
+2. npm 5.2+
+
+
+
+Run the following in your terminal/console:
+```console
+npm start
+```
+Project will run at http://localhost:3000/
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const webpack = require("webpack");
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        bundle: './src/js/index.js',
+    },
+    mode: 'development',
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /(\.css|\.scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin('bundle.css'),
+        new webpack.HotModuleReplacementPlugin(),
+        new StyleLintPlugin({
+            configFile: '.stylelintrc'
+        }),
+        new FriendlyErrorsWebpackPlugin()
+    ],
+    devtool: 'eval-cheap-module-source-map'
+};
